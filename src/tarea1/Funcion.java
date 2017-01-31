41c4ef9be57822d7067e2f5ac9148a4583b97dac
Fernando Partida Milanés <ferpart@hotmail.com>
2017-01-31 12:17:45 -0600

    Estructura de datos

---
+++ src/tarea1/Funcion.java
@@ -0,0 +1,25 @@
+package tarea1;
+
+public class Funcion {
+
+	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+		int reps = 10000000;
+		operation(reps);
+		total_time(startTime);
+	}
+	
+	public static void operation (int reps){
+		for (int i=0 ; i<reps ; i++){
+			int result=((i*(i+1))/2);
+			System.out.println(result);
+		}	
+	}
+
+	public static void total_time(long startTime){
+		long endTime = System.nanoTime();
+		long time=(endTime - startTime);
+		double seconds = (double)time / 1000000000.0;
+		System.out.println("Took "+(seconds) + " s"); 		
+	}
+}
